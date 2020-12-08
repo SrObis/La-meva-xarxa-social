c228de4805f97ca4b740bf0ca92af08cbd7cc8a5
LxEvan <cristianctl1@gmail.com>
2020-12-08 19:42:35 +0100

    Update master en mi branca

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistresComponent } from './components/registres/registres.component';
-import { ListaUsuarisComponent } from './components/lista-usuaris/lista-usuaris.component';
-import { DetallUsuarisComponent } from './components/detall-usuaris/detall-usuaris.component';
+import { ListaUsuarisComponent } from './components/perfils/lista-usuaris/lista-usuaris.component';
+import { DetallUsuarisComponent } from './components/perfils/detall-usuaris/detall-usuaris.component';
 import { AdministradorComponent } from './components/administrador/administrador.component';
+import { PerfilsComponent } from './components/perfils/perfils.component';
 
 @NgModule({
   declarations: [
@@ -16,14 +17,13 @@ import { AdministradorComponent } from './components/administrador/administrador
     ListaUsuarisComponent,
     DetallUsuarisComponent,
     AdministradorComponent,
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule
+    PerfilsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
