d1a77dab7cc8dba729d5b5ef610b960517bf9c63
Malsone <marcllabco@gmail.com>
2020-12-09 12:49:29 +0100

    V0.4 Arreglos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PerfilsComponent } from './components/perfils/perfils.component';
 import { RegistresComponent } from './components/registres/registres.component';
 import { ErrorComponent } from './components/error/error.component';
+//import { AdministradorComponent } from './components/administrador/administrador.component';
+
 
 const routes: Routes = [
+  { path: 'perfils', component: PerfilsComponent },
   { path: 'registres', component: RegistresComponent },
+  //{ path: 'administrador', component: AdministradorComponent },
   { path: 'error', component: ErrorComponent }, //pagina error
   { path: '**' , redirectTo: '/error'}
 ];
